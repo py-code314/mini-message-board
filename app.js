d791e336384efd12b52474844c3945090a89b380
srikk <122169588+py-code314@users.noreply.github.com>
2026-01-23 13:26:39 -0500

    style: Add styles to index page

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const app = express()
 const path = require('node:path')
+const indexRouter = require('./routes/indexRoutes')
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 const assetsPath = path.join(__dirname, 'public')
-app.set(express.static(assetsPath))
+app.use(express.static(assetsPath))
 
 app.use(express.urlencoded({ extended: true }))
+
+app.use('/', indexRouter)
 
 const PORT = 3000
 app.listen(PORT, (error) => {
