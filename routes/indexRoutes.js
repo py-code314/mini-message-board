3dfd30c3980382dcebb4443cee2f61cc3379fa60
srikk <122169588+py-code314@users.noreply.github.com>
2026-01-23 21:04:47 -0500

    feat: Add message details route

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -3,20 +3,32 @@ const indexRouter = express.Router()
 
 const messages = [
   {
+    id: 1,
     text: 'Hi there!',
     user: 'Amando',
     added: new Date(),
   },
   {
+    id: 2,
     text: 'Hello World!',
     user: 'Charles',
     added: new Date(),
   },
 ]
 
-
 indexRouter.get('/', (req, res) => {
   res.render('pages/index', { title: 'Home', messages })
+})
+
+indexRouter.get('/:id', (req, res) => {
+  const msgId = req.params.id
+  console.log('id:', msgId)
+  const message = messages.find((message) => message.id === Number(msgId))
+  console.log('message:', message)
+  if (!message) {
+    console.log('Error: message not found')
+  }
+  res.render('pages/msgDetails', { title: 'Message', message })
 })
 
 module.exports = { indexRouter, messages }
