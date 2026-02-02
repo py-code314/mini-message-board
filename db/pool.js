f84f217230526029719a8ecc806395cbbe525658
srikk <122169588+py-code314@users.noreply.github.com>
2026-02-02 10:39:31 -0500

    feat: Connect to the database by installing `node-postgres`
    - Set up postgres database locally and create a table called messages.
    - Connect to the database through a connection string defined in `pool.js`.

---
+++ db/pool.js
@@ -0,0 +1,18 @@
+const { Pool } = require('pg')
+
+
+// Use env variables instead of hard coding
+// module.exports = new Pool({
+//   host: process.env.DATABASE_HOST || 'localhost',
+//   user: process.env.DATABASE_USER,
+//   database: process.env.DATABASE_NAME,
+//   password: process.env.DATABASE_PASSWORD,
+//   port: process.env.DATABASE_PORT || 5432,
+// })
+
+
+// Use env variables instead of hard coding
+module.exports = new Pool({
+  connectionString: `postgresql://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`,
+})
+
